95e644b594b3b6c31d08d4622d62b0b11f765973
Tanishq Aryan <100710545+tanishqaryan9@users.noreply.github.com>
2026-07-17 14:08:47 +0530

    Remove trailing slash from API_BASE URL

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://springai-learning.onrender.com/";
+const API_BASE = "https://springai-learning.onrender.com";
 
 function authHeaders() {
   const token = localStorage.getItem("token");
